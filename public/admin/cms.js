c36fb6a0cfe7155a46f4b3a9dcba4a7412497ad9
Nelson Garces <nelsonrgarces@gmail.com>
2020-09-21 02:15:06 -0400

    hopefully adding points to CMS part 2

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae9696f1713b869ccc20";
+/******/ 	var hotCurrentHash = "68f0ffd743dc021634b6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -88051,7 +88051,7 @@ var _default = useSiteMetadata;
 
 // extracted by mini-css-extract-plugin
     if(true) {
-      // 1600655676149
+      // 1600667013869
       var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
